9c66c8e2c816eee778f0aab414451e9d8fffcaf3
Michael Palmer <palmer.mikepalmer@gmail.com>
2023-08-09 12:56:10 -0500

    add css status indicator to each guess

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ guessIndex, guesses }) {
+function Guess({ guessIndex, guesses, answer }) {
+  const currentGuess = guesses[guessIndex];
   return (
     <p className='guess'>
-      {guesses[guessIndex]
-        ? Array.from(guesses[guessIndex]).map((letter, idx) => (
-            <span key={`guess-letter-${idx}`} className='cell'>{letter}</span>
+      {currentGuess
+        ? checkGuess(currentGuess, answer).map(({letter, status}, idx) => ( 
+            <span key={`guess-letter-${idx}`} className={`cell ${status}`}>{letter}</span>
         ))
         : range(5).map((num) => (
             <span key={`empty-letter-${num}`} className='cell'></span>
