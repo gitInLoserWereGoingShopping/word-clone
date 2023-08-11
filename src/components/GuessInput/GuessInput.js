884f9704bd5797ca40a4c57f94ffaf6320bfabe3
phoenixtoots <palmer.mikepalmer@gmail.com>
2023-08-11 15:39:15 -0500

    hotfix: add emojis, no spaces allowed for input guess

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function GuessInput({ guesses, setGuesses }) {
     const [guess, setGuess] = React.useState('');
-    const handleSubmit = (e) => {
+    const handleSubmit = (e) => { 
         e.preventDefault();
         if (guess.length !== 5) {
-            alert('Please provide a 5 letter guess');
+            alert('Please provide a 5 letter guess 💖');
         }
         else if (guesses.includes(guess)) {
-            alert('Already guessed that, try a new 5 letter guess');
+            alert('Already guessed that, try a new 5 letter guess 😺');
         }
         else {
             console.info({ guess });
@@ -17,12 +17,24 @@ function GuessInput({ guesses, setGuesses }) {
         }
     };
     const handleChange = (e) => {
-        setGuess(e.target.value.toUpperCase());
+        const nextGuess = e.target.value.toUpperCase();
+        const lastCharEntered = nextGuess[nextGuess.length - 1];
+        if (lastCharEntered !== ' ') {
+            setGuess(nextGuess);
+        }
     };
     return (
         <form id='guess-input-wrapper' onSubmit={handleSubmit}>
             <label htmlFor='guess-input'>Enter guess:</label>
-            <input id='guess-input' type='text' value={guess} onChange={handleChange}/>
+            <input
+                id='guess-input'
+                type='text'
+                required
+                minLength={5}
+                maxLength={5}
+                value={guess}
+                onChange={handleChange}
+            />
         </form>
     )
 }
